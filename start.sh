59e1b88cf308ed1537ebf1bd94115f8e2d136998
pjjacob98 <97329107+pjjacob98@users.noreply.github.com>
2022-05-17 15:47:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 git clone https://github.com/pjjacob98/test2
 cd /usr/src/app/test2
 pip3 install pyTelegramBotApi
-pip3 install flask
 python3 pyTelegramShellBot.py
-python3 pyTelegramShellBot.py &
